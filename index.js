29f28b40753e4d088ccbafaf985b7acd15302629
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-15 12:30:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,23 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+  time = time.split(":");
+  time = parseInt(time[0]);
+  if (time <= 12) {
+      return  "Good Morning";
+  } else if ( time > 12 && time <= 17) {
+      return  "Good Afternoon";
+  } else {
+      return "Good Evening";
+  }
+ const displayMessage = ("") => {
+   document.getElementById("greeting").innerText = "";
+ }
+}
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+// const displayMessage = () => {
+//
+// }
